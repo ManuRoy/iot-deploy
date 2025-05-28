19a99e36885bd6427cbdbb8d17c61dfd255b1fcf
ManuRoy <83333663+ManuRoy@users.noreply.github.com>
2025-05-29 01:17:14 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,43 @@
-#!/bin/bash
+pipeline {
+    agent any
 
-# Deploy Node-RED flow (replace URL if different)
-curl -X POST http://localhost:1880/flows -H "Content-Type: application/json" -d @node-red/flow.json
+    stages {
+        stage('Deploy Node-RED Flow') {
+            steps {
+                sh 'curl -X POST http://localhost:1880/flows -H "Content-Type: application/json" -d @node-red/flow.json'
+            }
+        }
 
-# Copy Logstash config
-cp logstash/rabbitmq-to-es.conf /etc/logstash/conf.d/rabbitmq-to-es.conf
+        stage('Configure Logstash') {
+            steps {
+                sh '''
+                cp logstash/rabbitmq-to-es.conf /etc/logstash/conf.d/rabbitmq-to-es.conf
+                systemctl restart logstash
+                '''
+            }
+        }
 
-# Restart Logstash to apply config
-systemctl restart logstash
+        stage('Register Elasticsearch Index Template') {
+            steps {
+                sh '''
+                curl -k -u elastic:PlnLz35OqHQ1UAOLqo8b -X PUT "https://localhost:9200/_index_template/sensor_template" \
+                -H "Content-Type: application/json" \
+                -d @elasticsearch/index-template.json
+                '''
+            }
+        }
 
-# Register index template
-curl -k -u elastic:PlnLz35OqHQ1UAOLqo8b -X PUT "https://localhost:9200/_index_template/sensor_template" \
--H 'Content-Type: application/json' \
--d @elasticsearch/index-template.json
+        stage('Inject Sample Data via Node-RED') {
+            steps {
+                // Trigger the inject node programmatically
+                sh 'curl -X POST http://localhost:1880/inject/bdf4c11c1aaee942'
+            }
+        }
+
+        stage('Verify Index Created') {
+            steps {
+                sh 'curl -k -u elastic:PlnLz35OqHQ1UAOLqo8b https://localhost:9200/_cat/indices?v'
+            }
+        }
+    }
+}
