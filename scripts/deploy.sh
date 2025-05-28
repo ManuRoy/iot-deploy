0de0d2ddf32f71dece7be4dea2e3a0eb195b7c61
ManuRoy <83333663+ManuRoy@users.noreply.github.com>
2025-05-29 01:22:37 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,43 +1,18 @@
-pipeline {
-    agent any
+#!/bin/bash
 
-    stages {
-        stage('Deploy Node-RED Flow') {
-            steps {
-                sh 'curl -X POST http://localhost:1880/flows -H "Content-Type: application/json" -d @node-red/flow.json'
-            }
-        }
+# Deploy Node-RED flow
+curl -X POST http://localhost:1880/flows \
+  -H "Content-Type: application/json" \
+  -d @node-red/flow.json
 
-        stage('Configure Logstash') {
-            steps {
-                sh '''
-                cp logstash/rabbitmq-to-es.conf /etc/logstash/conf.d/rabbitmq-to-es.conf
-                systemctl restart logstash
-                '''
-            }
-        }
+# Configure Logstash
+cp logstash/rabbitmq-to-es.conf /etc/logstash/conf.d/
+systemctl restart logstash
 
-        stage('Register Elasticsearch Index Template') {
-            steps {
-                sh '''
-                curl -k -u elastic:PlnLz35OqHQ1UAOLqo8b -X PUT "https://localhost:9200/_index_template/sensor_template" \
-                -H "Content-Type: application/json" \
-                -d @elasticsearch/index-template.json
-                '''
-            }
-        }
+# Register Elasticsearch index template
+curl -k -u elastic:PlnLz35OqHQ1UAOLqo8b -X PUT "https://localhost:9200/_index_template/sensor_template" \
+  -H "Content-Type: application/json" \
+  -d @elasticsearch/index-template.json
 
-        stage('Inject Sample Data via Node-RED') {
-            steps {
-                // Trigger the inject node programmatically
-                sh 'curl -X POST http://localhost:1880/inject/bdf4c11c1aaee942'
-            }
-        }
-
-        stage('Verify Index Created') {
-            steps {
-                sh 'curl -k -u elastic:PlnLz35OqHQ1UAOLqo8b https://localhost:9200/_cat/indices?v'
-            }
-        }
-    }
-}
+# Inject sample data via Node-RED
+curl -X POST http://localhost:1880/inject/bdf4c11c1aaee942
